e4aeb79007ae7547efc410456c06e21301f27a79
Sparx27 <nicolasgcardani@gmail.com>
2023-07-25 10:29:27 -0300

    fixed filter

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -11,13 +11,13 @@ const Filters = ({ filter, setFilter }) => {
             onChange={(e) => setFilter(e.target.value)}
           >
             <option value=''>-- All --</option>
-            <option value='ahorro'>Saving</option>
-            <option value='salud'>Health</option>
-            <option value='casa'>House</option>
-            <option value='comida'>Food</option>
-            <option value='ocio'>Leisure</option>
-            <option value='suscripciones'>Subscriptions</option>
-            <option value='gastos'>Other</option>  
+            <option value='saving'>Saving</option>
+            <option value='health'>Health</option>
+            <option value='house'>House</option>
+            <option value='food'>Food</option>
+            <option value='leisure'>Leisure</option>
+            <option value='subscriptions'>Subscriptions</option>
+            <option value='other'>Other</option>  
           </select>
         </div>
       </form>
